0720d4445c1cde64774dca8365a8d4ad5f0e59e5
Terry Fleury <tfleury@illinois.edu>
2020-09-29 11:21:12 -0500

    Update run.sh to use 'latest' tagged version.

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ docker run -d --name shib-idp -h idp.ncsa.illinois.edu \
   -v /opt/ncsa-shib-idp/config/etc/grid-security/hostkey.pem:/etc/grid-security/hostkey.pem \
   -v /opt/ncsa-shib-idp/credentials/shib-idp:/opt/shibboleth-idp/credentials \
   -v /opt/ncsa-shib-idp/credentials/tomcat:/opt/certs \
-ncsa/shib-idp
+ncsa/shib-idp:latest
