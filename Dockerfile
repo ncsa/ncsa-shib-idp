63aafa710e28a0c917cc5760f534cb2dbf204333
Terry Fleury <tfleury@illinois.edu>
2023-05-24 16:19:04 -0500

    CACC-465 Update shib-idp docker image to v1.8.2
    After updating the Shibboleth IdP DuoOIDC plugin, also remove the
    cronjob and associated files which attempted to generate CRLs for the
    long retired myproxy2013 (myproy.xsede.org) and tfca2013
    (tfca.ncsa.illinois.edu) CAs.

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,5 @@
 FROM i2incommon/shib-idp:latest
 
-# Need myproxy-logon and openssl for crl cronjob.
-RUN yum -y update && yum -y install myproxy openssl && yum -y clean all
-
-COPY config/etc /etc
 COPY config/shib-idp /opt/shibboleth-idp
 COPY config/tomcat /usr/local/tomcat
 COPY VERSION /usr/local/tomcat
--- run.sh
+++ run.sh
@@ -2,7 +2,6 @@ docker run -d --name shib-idp -h idp.ncsa.illinois.edu \
   -p 80:80 -p 443:443 --log-driver=syslog --restart always \
   --dns 141.142.2.2 --dns 141.142.230.144 \
   -v /opt/ncsa-shib-idp/config/tomcat/server.xml:/usr/local/tomcat/conf/server.xml \
-  -v /opt/ncsa-shib-idp/config/etc/grid-security/hostkey.pem:/etc/grid-security/hostkey.pem \
   -v /opt/ncsa-shib-idp/credentials/shib-idp:/opt/shibboleth-idp/credentials \
   -v /opt/ncsa-shib-idp/credentials/tomcat:/opt/certs \
 ncsa/shib-idp:latest
